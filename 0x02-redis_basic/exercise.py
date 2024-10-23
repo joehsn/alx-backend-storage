745d60bbac227d56946f5e9c992bc34b7f996031
joehsn <joehsn@outlook.com>
2024-10-23 20:10:13 +0000

    2. Incrementing values task is done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,37 +1,58 @@
 #!/usr/bin/env python3
 
 """
-    0. Writing strings to Redis tasks's module.
+0. Writing strings to Redis tasks's module.
+1. Reading from Redis and recovering original type task's module.
+2. Incrementing values task's module.
 """
 
 import uuid
 import redis
 from typing import Union, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Counts the number of calls made to a method in a Cache class.
+    """
+    @wraps(method)
+    def invoker(*args):
+        """
+        Increments the call counter of the the given method.
+        """
+        if args[0]._redis.exists(method.__qualname__) == 0:
+            args[0]._redis.set(method.__qualname__, 1)
+        else:
+            args[0]._redis.incr(method.__qualname__, 1)
+            return method(*args)
+        return invoker
 
 
 class Cache:
     """
-        Redis cache store.
+    Redis cache store.
     """
     def __init__(self) -> None:
         """
-            Initializes a Cache instance.
+        Initializes a Cache instance.
         """
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-            Stores a key-value pair and returns the key.
+        Stores a key-value pair and returns the key.
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
 
         return key
 
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
         """
-            Retrieves a value from a Redis.
+        Retrieves a value from a Redis.
         """
         data = self._redis.get(key)
         if fn is not None:
@@ -40,13 +61,13 @@ class Cache:
 
     def get_str(self, key: str) -> str:
         """
-            Coarce the retrieved value into a string.
+        Coarce the retrieved value into a string.
         """
         return self.get(key, lambda x: x.decode('utf-8'))
 
     def get_int(self, key: str) -> int:
         """
-            Coarce the retrieved value into a intger.
+        Coarce the retrieved value into a intger.
         """
         return self.get(key, lambda x: int(x))
 
